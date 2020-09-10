e9ed6bc09ede90ce76d0ed04ad4ef203d7eb821f
NguyenThanhDinh <65837993+NguyenThanhDinh2810@users.noreply.github.com>
2020-09-10 12:35:13 +0700

    update nav
    add js, add darkmod and fix small bugs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-window.addEventListener('scroll',  function(){
-    var nav = document.querySelector('nav');
-    nav.classList.toggle('sticky', window.scrollY > 0);
+window.addEventListener("scroll", function () {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
+$(document).ready(function () {
+  $(".click__register").click(function () {
+    $(".modal").css("display", "block");
+    $(".auth-form__register").css("display", "block");
+    $(".auth-form__login").css("display", "none");
+    $("input:checked").change();
+    document.getElementById("nav-moblie-input-btn").checked = false;
+  });
+  $(".click__login").click(function () {
+    $(".modal").css("display", "block");
+    $(".auth-form__login").css("display", "block");
+    $(".auth-form__register").css("display", "none");
+    document.getElementById("nav-moblie-input-btn").checked = false;
+  });
+  $(".click__exit").click(function () {
+    $(".modal").css("display", "none");
+  });
+  $(".nav-pc__mode-btn i").click(function () {
+    $(".nav-pc__mode-btn i").show();
+    $(this).hide();
+    $("body").toggleClass("dark-scheme night");
+  });
 });
